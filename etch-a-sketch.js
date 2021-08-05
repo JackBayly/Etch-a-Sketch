34e62a410f5e6e52326e838a172058321cf12124
Jack <jackohara96@gmail.com>
2021-08-05 11:38:22 -0400

    Etch-a-Scetch project for the Odin Project

---
+++ etch-a-sketch.js
@@ -0,0 +1,50 @@
+const container = document.getElementById("container");
+
+//Makes Grid
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+  
+    cell.addEventListener("mouseover", function( event ) {
+       
+        event.target.style.backgroundColor = "black";},); 
+        
+
+
+
+    container.appendChild(cell).className = "grid-item";
+    
+  };
+};
+
+
+
+
+
+//Adjust grid cell numbers button
+let gridNums= document.getElementById("input").value;
+
+if (gridNums<=100) {
+
+let h= document.getElementById("btninput");
+h.addEventListener("click",
+    makeRows(gridNums,gridNums),
+ );
+}
+ function refreshPage(){
+  window.location.reload();
+} 
+
+
+// Clear Button
+let btn =document.getElementById("btn");
+let grid= document.querySelectorAll(".grid-item");
+function style (){for (i = 0; i < (gridNums*gridNums); i++) {
+  grid[i].style.backgroundColor = "white";
+}};
+let k= btn.addEventListener("click", style)
+
+
+
